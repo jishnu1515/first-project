e9d9bf2d14bb73751c24e523ab2d983bdd601393
jishnu vardhan <12914736-9494757795@users.noreply.replit.com>
2024-08-26 16:53:26 +0000

    Initial commit

---
+++ jishnu.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import "./App.css";
+
+export default function App() {
+  return (
+    <main>
+      <h1>My First React Project!</h1>
+      <p>This is a simple React application built on Replit.</p>
+    </main>
+  );
+}
